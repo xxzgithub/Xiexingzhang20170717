0e18ae0d5f74144280f02d01c2a410acf89fe486
xxz <xiedi315@163.com>
2017-07-17 10:32:29 +0800

    淘宝购物车

---
+++ app/src/main/java/com/example/asus/xiexingzhang20170717/entity/GroupInfo.java
@@ -0,0 +1,16 @@
+package com.example.asus.xiexingzhang20170717.entity;
+
+public class GroupInfo extends BaseInfo
+{
+	public GroupInfo()
+	{
+		super();
+	}
+
+	public GroupInfo(String id, String name)
+	{
+		super(id, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
